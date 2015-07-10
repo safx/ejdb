5fc16655d10284879f9f276971a41cf8f9095c50
Anton Adamansky <adamansky@gmail.com>
2015-07-10 15:24:59 +0600

    EJDB documentation updated

--- _static/theme.js
+++ _static/theme.js
@@ -1,6 +1,6 @@
 window.DOCUMENTATION_OPTIONS = {
     URL_ROOT : '',
-    VERSION : '1.2.8',
+    VERSION : '1.2.9',
     COLLAPSE_INDEX : false,
     FILE_SUFFIX : '.html',
     HAS_SOURCE : 'true' === 'true'
